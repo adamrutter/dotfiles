02d41cb2162990da4e808f461273d4299b755eb1
adamrutter <adamrutter92@gmail.com>
2018-10-27 19:21:38 +0100

    Made Firefox open on workspace 1

--- fish.functions/fish_prompt.fish
+++ fish.functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     set_color $fish_color_cwd
-    set_color green
+    set_color red 
     set -U fish_prompt_pwd_dir_length 0
     echo -n "<$USER "
     echo -n (prompt_pwd)"> "
