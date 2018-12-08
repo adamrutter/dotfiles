425b45f9f15dacc4acc6d8c05e11c43a6c31f073
adamrutter <adamrutter92@gmail.com>
2018-12-08 15:44:30 +0000

    Removed some text from polybar

--- scripts/openweathermap-detailed.sh
+++ scripts/openweathermap-detailed.sh
@@ -44,9 +44,9 @@ else
 fi
 
 if [ ! -z "$weather" ]; then
-    weather_desc=$(echo "$weather" | jq -r ".weather[0].main")
+    # weather_desc=$(echo "$weather" | jq -r ".weather[0].main")
     weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
     weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
 
-    echo "$(get_icon "$weather_icon")" "$weather_desc", "$weather_temp$SYMBOL"
+    echo "$(get_icon "$weather_icon")" "$weather_desc""$weather_temp$SYMBOL"
 fi
