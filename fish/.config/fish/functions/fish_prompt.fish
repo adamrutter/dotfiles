71a4b18af68733180f77da107b649c003aedbe28
adamrutter <46485756+adamrutter@users.noreply.github.com>
2019-02-25 18:18:04 +0000

    Added signifier to prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -3,6 +3,6 @@ function fish_prompt
     set_color green
     set -U fish_prompt_pwd_dir_length 0
     echo -n "<$USER "
-    echo -n (prompt_pwd)"> "
+    echo -n (prompt_pwd)"> fish "
     echo -e \a
 end
