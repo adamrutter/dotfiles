835d666d7ac04cd567e5be18f73e835dcc704440
adamrutter <46485756+adamrutter@users.noreply.github.com>
2019-07-30 22:49:46 +0100

    Added spotify

---
+++ polybar/.config/polybar/scripts/spotify/play-pause.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+while true;do
+	player_status=$(playerctl status 2> /dev/null)
+
+	if [ "$player_status" = "Playing" ]; then
+	    echo "" 
+	elif [ "$player_status" = "Paused" ]; then
+	    echo ""
+	else
+	    echo ""
+	fi
+	sleep .1 &
+	wait
+done
