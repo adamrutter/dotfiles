67919dd38c390aa72ca99cb9b63482bf4f0c84f7
adamrutter <46485756+adamrutter@users.noreply.github.com>
2019-08-07 01:17:41 +0100

    Made scripts only apply to spotify

--- polybar/.config/polybar/scripts/spotify/prev.sh
+++ polybar/.config/polybar/scripts/spotify/prev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true;do
-	player_status=$(playerctl status 2> /dev/null)
+	player_status=$(playerctl --player=spotify status 2> /dev/null)
 
 	if [ "$player_status" = "Playing" ]; then
 	    echo "" 
