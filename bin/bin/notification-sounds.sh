8746f3abcfb339c485c5305640a9cd26f20863f9
Adam Rutter <adamrutter92@gmail.com>
2020-02-15 18:43:20 +0000

    Notification sounds for everything other than notify-send

---
+++ bin/bin/notification-sounds.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Only play a sound if the notification doesn't come from notify-send
+# For notify-send, play a sound in the script where it is called
+if [[ $1 != *"notify-send"* ]]; then
+  canberra-gtk-play -i message
+fi
