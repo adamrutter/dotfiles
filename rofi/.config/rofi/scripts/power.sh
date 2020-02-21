9e0b4bacd4975cc026a3a505cddcc6eb6f3bf204
Adam Rutter <adamrutter92@gmail.com>
2020-02-21 01:24:04 +0000

    Added modes to rofi

---
+++ rofi/.config/rofi/scripts/power.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Define options
+options=(
+  'Lock'
+  'Reboot'
+  'Poweroff'
+)
+
+if [[ -z "$@" ]]; then
+  # Print options
+  for i in "${options[@]}"; do
+    echo $i
+  done
+else
+  # Do options
+  case "$1" in
+    Lock)
+      pkill rofi
+      sleep 1
+      /home/adam/bin/lock.sh
+      ;;
+    Reboot)
+      systemctl reboot
+      ;;
+    Poweroff)
+      systemctl poweroff
+      ;;
+    *)
+      ;;
+  esac
+fi
