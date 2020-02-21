9d95ceadbc85857cfca9941beb93783ee2f37f0c
Adam Rutter <adamrutter92@gmail.com>
2020-02-21 01:29:18 +0000

    Removed previous image

--- bin/bin/lock.sh
+++ bin/bin/lock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm /tmp/screenshot.png > /dev/null 2>&1
 scrot /tmp/screenshot.png
 convert /tmp/screenshot.png -scale 10% -scale 1000% /tmp/screenshotblur.png
 i3lock -e -u -i /tmp/screenshotblur.png
