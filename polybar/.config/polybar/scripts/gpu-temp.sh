a765a23b46c0bae785b899c982473a8ae6152de4
Adam Rutter <adamrutter92@gmail.com>
2020-08-25 00:49:46 +0100

    Remove polybar

--- polybar/.config/polybar/scripts/gpu-temp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sensors | grep -i amd -A5 | grep temp1 | cut -d' ' -f 9 | cut -c 2- | rev | cut -c 6- | rev
-sensors | grep -i amd -A5 | grep edge | cut -d' ' -f 10 | cut -c 2- | rev | cut -c 6- | rev
