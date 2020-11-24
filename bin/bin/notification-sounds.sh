73df02c1df506c06b4e39f4a83727bc7e2ab4975
Adam Rutter <adamrutter92@gmail.com>
2020-11-24 17:30:57 +0000

    Remove unused scripts

--- bin/bin/notification-sounds.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# if [[ $1 != *"notify-send"* && $1 != *"dunstify"* ]]; then
-# if [[ $2 != "Volume notification" && $2 != "Screenshot" ]]; then
-if [[ $5 != *"LOW"* ]]; then
-  canberra-gtk-play -i message
-fi
