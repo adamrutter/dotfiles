35dc46a09af8eb2c4a626485fb1161805309c8eb
Adam Rutter <adamrutter92@gmail.com>
2021-04-17 12:10:58 +0100

    Adjust prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,8 +1,12 @@
 function fish_prompt
+
     set_color $fish_color_cwd
     set_color blue
-    set -U fish_prompt_pwd_dir_length 0
-    echo -n "<$USER "
-    echo -n (prompt_pwd)"> fish "
-    echo -e \a
+    set -U fish_prompt_pwd_dir_length 1
+
+    echo -n "$USER  "
+    echo -n ""(prompt_pwd)"  "
+    echo ""(fish_git_prompt %s)" "
+    set_color normal
+    echo "> "
 end
